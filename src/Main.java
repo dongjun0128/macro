23005199a89dd4a7730d99d7aa05316f2e2902ad
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2021-08-18 17:14:26 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,75 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.concurrent.TimeUnit;
+
+class key implements KeyListener{
+    @Override
+    public void keyPressed(KeyEvent e2) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e2) {}
+    public void keyReleased(KeyEvent e2){ }
+
+}
+
+
+public class Main {
+    JFrame frame = new JFrame("매크로");
+    Button btn1 = new Button("Start");
+    Button btn2 = new Button("End");
+
+    public void creatFrame(){
+        frame.getContentPane().setLayout(null);
+
+        btn1.setBounds(30,170,122,30);
+        btn2.setBounds(182,170,122,30);
+
+        frame.add(btn1);
+        frame.add(btn2);
+
+        frame.setSize(350,300);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void buttonAction(Button btn1, Button btn2){
+        btn1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Robot robot = new Robot();
+                    int i = 0;
+                    while(i<10){
+                        i++;
+                        robot.keyPress(32);
+                        System.out.println("실행");
+                        TimeUnit.SECONDS.sleep(1);
+                    }
+                }
+                catch (Exception e1){
+                    System.out.println(e1);
+                }
+
+            }
+        });
+
+        btn2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("bnt2 눌림");
+            }
+        });
+
+    }
+
+    public static void main(String[] args) {
+        Main frame = new Main();
+        frame.creatFrame();
+        frame.buttonAction(frame.btn1, frame.btn2);
+    }
+}
