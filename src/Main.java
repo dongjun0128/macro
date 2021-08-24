db185f9831c82573159cde662f49d90938cdf224
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2021-08-24 16:22:38 +0900

    [modify] 이미지 위치 수정

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,22 +25,40 @@ public class Main extends JFrame {
     JFrame frame = new JFrame("매크로");
     Button btn1 = new Button("Start");
     Button btn2 = new Button("End");
-    ImageIcon img1;
-    ImageIcon img2;
+    JPanel contentPane = new JPanel();
 
-
+    ImageIcon imageSetSize(ImageIcon icon, int i, int j) {
+    	Image ximg=icon.getImage();
+    	Image yimg=ximg.getScaledInstance(i,j,java.awt.Image.SCALE_SMOOTH);
+    	ImageIcon xyimg = new ImageIcon(yimg);
+    	return xyimg;
+    }
+    
     public void creatFrame(){
         frame.getContentPane().setLayout(null);
 
-        btn1.setBounds(30,170,122,30);
-        btn2.setBounds(182,170,122,30);
+        btn1.setBounds(30,200,122,30);
+        btn2.setBounds(182,200,122,30);
 
-        frame.add(btn1);
-        frame.add(btn2);
+        contentPane = new JPanel();
+        contentPane.setBackground(Color.white);
+        contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
-        img1 = new ImageIcon("images/picture1.png");
-        img2 = new ImageIcon("images/picture2.png");
+        contentPane.add(btn1);
+        contentPane.add(btn2);
 
+        ImageIcon [] images = {new ImageIcon("images/picture1.png"), new ImageIcon("images/picture2.png")};
+        
+        images[0]=imageSetSize(images[0],135,135);
+        
+        JLabel imgLabel = new JLabel(images[0]);
+        Container c = getContentPane();
+        c.setLayout(getLayout());
+        c.add(imgLabel);
+        
+        frame.setContentPane(contentPane);
         frame.setSize(350,300);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
